8d302fa2d87625cfdb0cdfe12f6a94e5af6baee2
Fernando Pulsa <fernandopulsa@gmail.com>
2019-02-25 18:11:52 +0100

    styles configurated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 import React from "react"
 // import { Link } from "gatsby"
 
-const Header = props => <header>this is a header</header>
+const Header = props => (
+  <header>
+    <div className="content_wrapper layout">
+      <p>this is a header</p>
+    </div>
+  </header>
+)
 
 export default Header
